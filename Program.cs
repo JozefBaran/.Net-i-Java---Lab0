751238a10a493543d727cc213c808284f58598c6
jozef <jozef@DESKTOP-N1MGSAJ>
2024-03-05 12:38:25 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class FizzBuzz
+    {
+        private int upperLim;
+
+        public FizzBuzz(int upperLim)
+        {
+            this.upperLim = upperLim;
+        }
+
+        public void PrintFizzBuzz()
+        {
+            for (int i = 1; i <= upperLim; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj górny zakres liczb dla FizzBuzz:");
+            int upperLim = Convert.ToInt32(Console.ReadLine());
+
+            FizzBuzz fizzBuzz = new FizzBuzz(upperLim);
+            fizzBuzz.PrintFizzBuzz();
+        }
+    }
+}
